eab635465886a25b26e0558a17cb8fcb02f76a44
easonblack <eason820720@163.com>
2018-06-27 16:34:20 +0800

    up

--- slim/simple/index.php
+++ slim/simple/index.php
@@ -5,6 +5,7 @@ use \Psr\Http\Message\ServerRequestInterface as Request;
 use \Psr\Http\Message\ResponseInterface as Response;
 
 require '../../vendor/autoload.php';
+require './mysql.php';
 
 $app = new Slim\App;
 
@@ -19,15 +20,11 @@ $app->get('/hello[/{name}]', function ($request, $response, $args) {
     return $response;
 })->setArgument('name', 'World!');
 
-$servername="localhost";
-$username = "root";
-$password = "root";
 
-$app->get('/demo', function($request, $response, $args) {
-	global $servername,$username, $password;
-
+$app->get('/user', function($request, $response, $args) {
+	
     try {
-		$conn = new PDO("mysql:host=$servername;dbname=demo", $username, $password);
+		$conn = connect_db();
         $stmt = $conn->query("select * from user");
 		$stmt->execute();
         $names = $stmt->fetchAll(PDO::FETCH_OBJ);
@@ -35,6 +32,29 @@ $app->get('/demo', function($request, $response, $args) {
     } catch(PDOException $e) {
         echo  '{"error":{"text":'. $e->getMessage() .'}}';
     }
+});
+
+$app->get('/user/{id}', function($request, $response, $args) {
+    try {
+        $conn = connect_db();
+        $id = $args['id'];
+        $sql = "SELECT * FROM user WHERE `id` = '$id'";
+        $stmt = $conn->query($sql);
+		$stmt->execute();
+        $result = $stmt->fetchAll(PDO::FETCH_OBJ);
+        echo   json_encode($result);
+    } catch(PDOException $e) {
+        echo  '{"error":{"text":'. $e->getMessage() .'}}';
+    }
+});
+
+$app->post('/user', function($request, $response, $args){
+    $form = $request->getParsedBody();
+    $conn = connect_db();
+    $sql = "insert into user (name,role)"
+            . " VALUES('$form[name]','$form[role]')";
+    $exe = $conn->query($sql);
+    echo true;
 });
 
 $app->run();
