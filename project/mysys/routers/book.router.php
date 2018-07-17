e67e9791948b6c92f9c3b09cf81e68e7dd3bb38e
EasonBlack <eason820720@163.com>
2018-07-17 23:07:54 +0800

    up

--- project/mysys/routers/book.router.php
+++ project/mysys/routers/book.router.php
@@ -11,4 +11,18 @@
             echo  '{"error":{"text":'. $e->getMessage() .'}}';
         }  
     });
+
+    $app->get('/book/{id}', function ($request, $response, $args) use ($app) {	
+        try {
+            $id = $args['id'];
+            $dbconn = Core::getInstance();
+            $stmt =  $dbconn->dbh->query("select * from BOOK where ID='$id'");
+            
+            $stmt->execute();
+            $results = $stmt->fetchAll(PDO::FETCH_OBJ);
+            echo json_encode($results);
+        }  catch(PDOException $e) {
+            echo  '{"error":{"text":'. $e->getMessage() .'}}';
+        }  
+    });
 ?>
