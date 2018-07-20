49491c5e2803b23fa10e4ed3e967e55d44fdc90c
easonblack <eason820720@163.com>
2018-07-20 13:23:29 +0800

    up

--- project/mysys/routers/charactor.router.php
+++ project/mysys/routers/charactor.router.php
@@ -13,6 +13,34 @@
         }  
     });
 
+    $app->get('/charactor/{id}', function ($request, $response, $args) use ($app) {	
+        try {
+            $id = $args['id'];
+            $dbconn = Core::getInstance();
+            $sql = "select * from CHARACTOR_CONTENT WHERE CID='$id'";
+            $stmt =  $dbconn->dbh->query($sql);       
+            $stmt->execute();
+            $results = $stmt->fetchAll(PDO::FETCH_OBJ);
+            echo json_encode($results);
+        }  catch(PDOException $e) {
+            echo  '{"error":{"text":'. $e->getMessage() .'}}';
+        }  
+    });
+
+    $app->get('/charactor-content/{id}', function ($request, $response, $args) use ($app) {	
+        try {
+            $id = $args['id'];
+            $dbconn = Core::getInstance();
+            $sql = "select * from CHARACTOR_CONTENT WHERE ID='$id'";
+            $stmt =  $dbconn->dbh->query($sql);       
+            $stmt->execute();
+            $results = $stmt->fetchAll(PDO::FETCH_OBJ);
+            echo json_encode($results);
+        }  catch(PDOException $e) {
+            echo  '{"error":{"text":'. $e->getMessage() .'}}';
+        }  
+    });
+
 
     $app->post('/book/{id}/charactor', function ($request, $response, $args) use ($app) {	
         $form = $request->getParsedBody();
@@ -37,5 +65,43 @@
         $dbconn->dbh->query($sql);
         
         echo true;
+    });
+
+
+    $app->post('/charactor/{id}', function ($request, $response, $args) use ($app) {	
+        try {
+            $id = $args['id'];
+            $form = $request->getParsedBody();
+
+            $dbconn = Core::getInstance();
+
+            $sql = "insert into CHARACTOR_CONTENT (CID,NAME,CONTENT)"
+            . " VALUES('$id', '$form[name]', '$form[content]')";
+            //echo $sql;
+            $stmt =  $dbconn->dbh->query($sql);       
+          
+            echo true;
+        }  catch(PDOException $e) {
+            echo  '{"error":{"text":'. $e->getMessage() .'}}';
+        }  
+    });
+
+    $app->put('/charactor/{cid}/{id}', function ($request, $response, $args) use ($app) {	
+        try {
+            $cid = $args['cid'];
+            $id = $args['id'];
+            $form = $request->getParsedBody();
+
+            $dbconn = Core::getInstance();
+
+            $sql = "update CHARACTOR_CONTENT " 
+            . " set NAME='$form[name]',`CONTENT`='$$form[content]'" 
+            . " where id='$id'";
+
+            $stmt =  $dbconn->dbh->query($sql);       
+            echo true;
+        }  catch(PDOException $e) {
+            echo  '{"error":{"text":'. $e->getMessage() .'}}';
+        }  
     });
 ?>
